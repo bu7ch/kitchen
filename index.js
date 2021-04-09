df22cf44cf49a245eea909229d0916e92d65e14e
Rodolphe <dutchr@live.fr>
2021-04-09 17:25:35 +0200

    :art: Adding 404 error message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { logErrors } = require('./controller/errorController');
+const { logErrors, respondNoRessourceFound } = require('./controller/errorController');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -8,6 +8,7 @@ const homeController = require('./controller/homeController')
 app.set('view engine', 'pug')
 app.set('views', './views')
 app.use(logErrors)
+app.use(respondNoRessourceFound)
 
 app.get("/name/:myName", homeController.respondWithMyName)
 
