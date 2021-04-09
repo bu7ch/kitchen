251960dc8fc00add2d702ff00d645d1d1858ca04
Rodolphe <dutchr@live.fr>
2021-04-09 17:16:07 +0200

    :sparkles: adding errorLogs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { logErrors } = require('./controller/errorController');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -6,7 +7,7 @@ const port = process.env.PORT || 3000;
 const homeController = require('./controller/homeController')
 app.set('view engine', 'pug')
 app.set('views', './views')
-
+app.use(logErrors)
 
 app.get("/name/:myName", homeController.respondWithMyName)
 
