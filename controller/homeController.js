378620c0812c42dd7bb083be0765fd1ba9db483e
LittleFootDev <thelittlefootdev@gmail.com>
2021-05-10 16:14:04 +0200

    refactorisation de l'aplication. ajout des route, ajout des fichier .pug

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,8 +1,36 @@
 exports.welcome = (req, res) => {
   res.render('index')
 };
-exports.respondWithMyName = (req, res) => {
-  let paramsName = req.params.myName;
-  res.render('index',{name: paramsName})
+const courses = [
+  {
+    title : 'comment faire des cakes',
+    coût : 50
+  },
+  {
+    title : 'comment faire des crêpes',
+    coût : 25
+  },
+  {
+    title : 'comment faire des pizzas',
+    coût : 10
+  },
+  {
+    title : 'comment faire des samousas',
+    coût : 73
+  },
+  {
+    title : 'comment faire des sushis',
+    coût : 125
+  }
+]
+
+exports.showCourses = (req, res ) => {
+  res.render('courses', {oCourses : courses });
 };
 
+exports.showSignUp = (req, res ) => {
+  res.render('contact');
+};
+exports.postedSignUpForm = (req, res ) => {
+  res.render('thanks');
+};
--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const homeRouter = require('./routes/homeRoute');
 const app = express();
 
 const port = process.env.PORT || 3000;
-
+app.use(express.static('public'));
 app.set('view engine', 'pug')
 app.set('views', './views')
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(logErrors)
 app.use('/', homeRouter);
 app.use(respondNoRessourceFound)
