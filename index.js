4168a70fb23f096f4148e540b07fe24ef5f29887
Rodolphe <dutchr@live.fr>
2021-05-29 20:26:53 +0200

    :sparkles: Ajout de la base de donnée avec MongoDB
    close #3

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const { logErrors, respondNoRessourceFound } = require('./controller/errorController');
 const homeRouter = require('./routes/homeRoute');
 const app = express();
+const mongoose = require('mongoose');
+const dbURL = "mongodb://localhost:27017/"
+const dbName ="kitchen_db"
+mongoose.Promise = global.Promise;
+mongoose.connect(dbURL + dbName ,{useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log(`[Mongo connected !!]`))
+  .catch(err => {
+    console.log(Error, err.message);
+  })
 
 const port = process.env.PORT || 3000;
 app.use(express.static('public'));
